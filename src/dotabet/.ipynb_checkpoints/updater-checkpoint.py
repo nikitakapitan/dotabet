78e139d6a2eb98cf86cc85b484629f348ef5a968
Nikita Kapchenko <kapchenko.nikita@gmail.com>
2024-07-28 14:42:41 +0200

    Auto push 2024-07-28 14:42:40

---
+++ src/dotabet/.ipynb_checkpoints/updater-checkpoint.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from dotabet import utils
+
+def process_team_compositions(features_csv_path, teams_csv_path):
+    # Load data
+    features_df = pd.read_csv(features_csv_path)
+    teams_df = pd.read_csv(teams_csv_path)
+    teams_df = teams_df[teams_df['top'] == 1]
+    int_cols = ['Pos1ID', 'Pos2ID', 'Pos3ID', 'Pos4ID', 'Pos5ID', 'team_composition_id']
+    teams_df[int_cols] = teams_df[int_cols].astype('Int64')
+
+    
+    for _, team_row in teams_df.iterrows():
+        team_id = team_row['team_id']
+        team_composition_id = team_row['team_composition_id']
+        team_csv_player_ids = {
+            team_row['Pos1']: int(team_row['Pos1ID']),
+            team_row['Pos2']: int(team_row['Pos2ID']),
+            team_row['Pos3']: int(team_row['Pos3ID']),
+            team_row['Pos4']: int(team_row['Pos4ID']),
+            team_row['Pos5']: int(team_row['Pos5ID'])
+        }
+
+        # Assert sum of values
+        assert sum(team_csv_player_ids.values()) == team_composition_id,\
+        f"Team composition ID mismatch for team {team_id}: sum csv = ({sum(team_csv_player_ids.values())}) VS team_composition_id ({team_composition_id})"
+
+        # Filter and sort features_df by start_time
+        team_matches = features_df[features_df['player_team_id'] == team_id]
+        if team_matches.empty:
+            continue
+
+        team_matches = team_matches.sort_values(by='start_time', ascending=False)
+        most_recent_match_id = team_matches.iloc[0]['match_id']
+        recent_match_players = team_matches[team_matches['match_id'] == most_recent_match_id].head(5)
+
+        assert len(recent_match_players) == 5, "Expected 5 players for the recent match"
+
+        # Create set of player IDs from the features
+        features_player_ids = set(recent_match_players['account_id'].astype(int))
+        
+        # Compute team_composition_id from features
+        computed_team_composition_id = sum(features_player_ids)
+        
+        if computed_team_composition_id != team_composition_id:
+            # Check differences
+            team_csv_player_ids_set = set(team_csv_player_ids.values())
+            diff_ids = team_csv_player_ids_set.symmetric_difference(features_player_ids)
+            print(f"DEBUG {diff_ids=} and {len(diff_ids)=}")
+            
+            if len(diff_ids) == 2:
+                active_id = list(features_player_ids - team_csv_player_ids_set).pop()
+                inactive_id = list(team_csv_player_ids_set - features_player_ids).pop()
+                pos_to_update = list(team_csv_player_ids.values()).index(inactive_id) + 1  # +1 to match Pos{x}ID format
+                pos_to_update_str = f'Pos{pos_to_update}ID'
+                teams_df.loc[teams_df['team_id'] == team_id, pos_to_update_str] = active_id
+                teams_df.loc[teams_df['team_id'] == team_id, f'Pos{pos_to_update}'] = utils.fetch_player_name(active_id)
+                print(f"Updated {pos_to_update_str} for team {team_id} with account_id {active_id} and name {utils.fetch_player_name(active_id)}")
+
+            else:
+                current_ids = team_csv_player_ids_set - features_player_ids
+                new_ids = features_player_ids - team_csv_player_ids_set
+                current_info = [(name, val) for name, val in team_csv_player_ids.items() if val in current_ids]
+                new_info = [(recent_match_players[recent_match_players['account_id'] == val]['player_name'].values[0], val) for val in new_ids]
+
+                print(f"Multiple differences found for team {team_row['team_name']} {team_id=}:")
+                print(f"Present in teams.csv but not in features: {current_info}🔚")
+                print(f"Present in features but not in teams.csv: {new_info}🆕")
+
+        else:
+            print(f"{team_row['team_name']}✅")
+    
+    # Save updated teams dataframe
+    teams_df.to_csv(r"D:\WORKSPACE\dotabet\constants\teams_upd.csv", index=False)
