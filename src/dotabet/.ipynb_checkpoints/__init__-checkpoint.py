425071883b26a28c6191d56594555076ee62a305
Nikita Kapchenko <kapchenko.nikita@gmail.com>
2024-07-29 15:27:29 +0200

    Auto push 2024-07-29 15:27:28

--- src/dotabet/.ipynb_checkpoints/__init__-checkpoint.py
+++ src/dotabet/.ipynb_checkpoints/__init__-checkpoint.py
@@ -15,5 +15,14 @@ except PackageNotFoundError:  # pragma: no cover
 finally:
     del version, PackageNotFoundError
 
-from dotabet import utils, filter, features, fetch, make_csv, cleanup, groupby, league, cumsum, transform, plot, checkout, vizual
+from dotabet import utils, filter, features, fetch, make_csv, cleanup, groupby, league, cumsum, transform, plot, checkout, vizual, train
 from dotabet.consistency import main
+
+input_files = [r"D:\WORKSPACE\dotabet\data" + fp for fp in [r"\1pro_games_2.json", r"\1pro_games_1.json"]]  
+pro_games_json_path = r"D:\WORKSPACE\dotabet\data\top_teams\1pro_games.json"
+all_player_csv_path = r"D:\WORKSPACE\dotabet\data\top_teams\all_players.csv"
+all_player_clean_csv_path = r"D:\WORKSPACE\dotabet\data\top_teams\all_players_clean.csv"
+features_csv_path = r"D:\WORKSPACE\dotabet\data\top_teams\features.csv"
+teams_csv_path = r"D:\WORKSPACE\dotabet\constants\teams.csv"
+
+train_path = r"D:\WORKSPACE\dotabet\data\train\matches.csv"
